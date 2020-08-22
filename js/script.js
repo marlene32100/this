7faa465efe93057fbd4bcaa88bd76eb7a2ec1906
marlene32100 <katia.debon@hotmail.it>
2020-08-22 20:03:56 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".box").on("click", function() {
+    	/**
+    	 * When we click on an element that has
+    	 * a 'box' class, this event will be fired.
+    	 */
+    	var classNames = $(this).attr("class").split(" ");
+        var boxClass = classNames[0];
+        var className = classNames[1];
+        if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+            $("."+className).css("background-color", "#000");
+        } else {
+            $("."+boxClass).css("background-color", "#000");
+            $("."+className).css("background-color", "red");
+        }
+    });
+});
